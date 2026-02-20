053651af6baea9930a3ca1917f738388ddc1651d
moroboshi-kun <166547588+moroboshi-kun@users.noreply.github.com>
2026-02-19 20:28:33 -0500

    Add file headers to project files

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
-## ----- backend.tf ----- ##
-# Configure the backend for Terraform state management
+# ===========================================
+# File: backend.tf
+# Date: 2026-02-19
+# Description: Terraform backend file
+#
+# ===========================================
 
 terraform {
   backend "s3" {}
---
+++ providers.tf
@@ -0,0 +1,7 @@
+# ===========================================
+# File: providers.tf
+# Date: 2026-02-19
+# Description: Terraform provider
+#               configuration
+#
+# ===========================================
